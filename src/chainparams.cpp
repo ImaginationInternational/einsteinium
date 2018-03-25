e68710e4ca59d4dc38453a188753857de035b16f
Zvi ben Yaakov <zvi.io.github@infosoc.net>
2018-03-25 20:13:37 +0300

    Minimal changes for syncing with forked chain

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -94,7 +94,8 @@ public:
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout = 1517356801; // January 31st, 2018
 
         // The best chain should have at least this much work.
-        consensus.nMinimumChainWork = uint256S("00000000000000000000000000000000000000000000000000c77bc63bfadbd1"); // <-- Einsteinium: OK
+//        consensus.nMinimumChainWork = uint256S("00000000000000000000000000000000000000000000000000c77bc63bfadbd1"); // <-- Einsteinium: OK
+        consensus.nMinimumChainWork = uint256S("0x00"); // <-- Einsteinium: OK
 
         /**
          * The message start string is designed to be unlikely to occur in normal data.
@@ -105,7 +106,7 @@ public:
         pchMessageStart[1] = 0xf1;  // <--Einsteinium: OK
         pchMessageStart[2] = 0xc4;  // <--Einsteinium: OK
         pchMessageStart[3] = 0xac;  // <--Einsteinium: OK
-        nDefaultPort = 41878;       // <--Einsteinium: OK
+        nDefaultPort = 31678;       // <--Einsteinium: OK
         nPruneAfterHeight = 100000;
 
         genesis = CreateGenesisBlock("NY Times 19/Feb/2014 North Korea Arrests Christian Missionary From Australia", CScript() << OP_DUP << OP_HASH160 << ParseHex("1cec44c9f9b769ae08ebf9d694c7611a16edf615") << OP_EQUALVERIFY << OP_CHECKSIG, 1392841423, 3236648, 0x1e0ffff0, 1, 50 * COIN); // <--Einsteinium: OK
@@ -114,15 +115,6 @@ public:
         assert(genesis.hashMerkleRoot == uint256S("0xb3e47e8776012ee4352acf603e6b9df005445dcba85c606697f422be3cc26f9b")); // <--Einsteinium: OK
 
         // Note that of those with the service bits flag, most only support a subset of possible options
-        vSeeds.push_back(CDNSSeedData("emc2.foundation", "dnsseeder01.emc2.foundation")); // <--Einsteinium: OK
-        vSeeds.push_back(CDNSSeedData("emc2.foundation", "dnsseeder02.emc2.foundation")); // <--Einsteinium: OK
-        vSeeds.push_back(CDNSSeedData("chainworksindustries.com", "emc2-seed01.chainworksindustries.com")); // <--Einsteinium: OK
-        vSeeds.push_back(CDNSSeedData("chainworksindustries.com", "emc2-seed02.chainworksindustries.com")); // <--Einsteinium: OK
-        vSeeds.push_back(CDNSSeedData("chainworksindustries.com", "emc2-seed03.chainworksindustries.com")); // <--Einsteinium: OK
-        vSeeds.push_back(CDNSSeedData("chainworksindustries.com", "emc2-seed04.chainworksindustries.com")); // <--Einsteinium: OK
-        vSeeds.push_back(CDNSSeedData("chainworksindustries.com", "emc2-seed05.chainworksindustries.com")); // <--Einsteinium: OK
-        vSeeds.push_back(CDNSSeedData("chainworksindustries.com", "emc2-seed06.chainworksindustries.com")); // <--Einsteinium: OK
-        vSeeds.push_back(CDNSSeedData("chainworksindustries.com", "emc2-seed07.chainworksindustries.com")); // <--Einsteinium: OK
 
         // Einsteinium: Address encoding P2SH BTC = 5 / LTC NEW = 50 / EMC2 OLD = 5
 
@@ -149,11 +141,12 @@ public:
             (    51365, uint256S("0x702b407c68091f3c97a587a8d92684666bb622f6821944424b850964b366e42c"))  // <--Einsteinium: OK
             (   621000, uint256S("0xe2bf6d219cff9d6d7661b7964a05bfea3128265275c3673616ae71fed7072981"))  // <--Einsteinium: OK
             (  1410100, uint256S("0xf6736ff2a7743014ab1902e442328f5c9928ce7f4edb2b4fd0130010cb4cebc4")), // <--Einsteinium: OK
-            1494147472, // * UNIX timestamp of last checkpoint block                                     // <--Einsteinium: OK
-            2147811,    // * total number of transactions between genesis and last checkpoint            // <--Einsteinium: OK
-                      //   (the tx=... number in the SetBestChain debug.log lines)
-            2000     // * estimated number of transactions per day after checkpoint                      // <--Einsteinium: OK
-            };
+//            1494147472, // * UNIX timestamp of last checkpoint block                                     // <--Einsteinium: OK
+//            2147811,    // * total number of transactions between genesis and last checkpoint            // <--Einsteinium: OK
+//                      //   (the tx=... number in the SetBestChain debug.log lines)
+//            2000     // * estimated number of transactions per day after checkpoint                      // <--Einsteinium: OK
+            0,0,0
+    };
     }
 };
 static CMainParams mainParams;
