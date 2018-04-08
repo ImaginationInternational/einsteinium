654abd4a9b9079bb0876881131132ec8a004e2e6
Zvi ben Yaakov <zvi.io.github@infosoc.net>
2018-04-08 21:16:30 +0300

    Bump to new Release version Added new Checkpoint

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -149,7 +149,7 @@ public:
             (    51365, uint256S("0x702b407c68091f3c97a587a8d92684666bb622f6821944424b850964b366e42c"))  // <--Einsteinium: OK
             (   621000, uint256S("0xe2bf6d219cff9d6d7661b7964a05bfea3128265275c3673616ae71fed7072981"))  // <--Einsteinium: OK
             (  1410100, uint256S("0xf6736ff2a7743014ab1902e442328f5c9928ce7f4edb2b4fd0130010cb4cebc4"))  // <--Einsteinium: OK
-            (  1671700, uint256S("0xbd0bce331b04666a4ed4c1d909aef40f89629d7af2a47820bfb6277275211f40")), // <--Imagination: OK
+            (  1671707, uint256S("0xc10478ec75f35a45fdce6fef40613748a9040e3dcbaa9f0c8ba89c2c5fd37435")), // <--Imagination: OK
 //            1494147472, // * UNIX timestamp of last checkpoint block                                     // <--Einsteinium: OK
 //            2147811,    // * total number of transactions between genesis and last checkpoint            // <--Einsteinium: OK
 //                      //   (the tx=... number in the SetBestChain debug.log lines)
