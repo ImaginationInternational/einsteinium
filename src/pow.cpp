8fccd67cf938f58d3b24b7b16207087450f4ff71
Zvi ben Yaakov <zvi.io.github@infosoc.net>
2018-04-17 20:48:04 +0300

    Removed some log spam

--- src/pow.cpp
+++ src/pow.cpp
@@ -145,7 +145,8 @@ bool CheckProofOfWork(uint256 hash, unsigned int nBits, const Consensus::Params&
 
     // Check proof of work matches claimed amount
     if (UintToArith256(hash) > bnTarget){
-        LogPrintf("CheckProofOfWork: does not match claim\n");
+    	//// commenting this out because of log spamming
+        // LogPrintf("CheckProofOfWork: does not match claim\n");
         return false;
     }
 
